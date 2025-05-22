3f05569505319bffa94c72c2d2484f54533e655b
Mohamed1995-github <110931927+Mohamed1995-github@users.noreply.github.com>
2025-05-22 12:16:15 +0200

    create

--- lib/screens/Medical/medical_provider.dart
+++ lib/screens/Medical/medical_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'package:medicall_app/Models/Medical/physicians_response.dart';
+import 'package:medicall_app/Models/Medical/specialties_response.dart';
 import 'package:medicall_app/NetworkManager/odoo_api_client.dart';
-import 'package:medicall_app/Models/Medical/specialty_model.dart';
-import 'package:medicall_app/Models/Medical/physician_model.dart';
 
 /// Provider pour gérer la récupération des spécialités et des médecins
 class MedicalProvider extends ChangeNotifier {
@@ -9,16 +9,16 @@ class MedicalProvider extends ChangeNotifier {
 
   bool _isLoadingSpecialties = false;
   bool _isLoadingPhysicians = false;
-  List<SpecialtyModel> _specialties = [];
-  List<PhysicianModel> _physicians = [];
+  SpecialtiesResponse? _specialties;
+  PhysiciansResponse? _physicians;
   String? _error;
 
   MedicalProvider({required OdooApiClient apiClient}) : _apiClient = apiClient;
 
   bool get isLoadingSpecialties => _isLoadingSpecialties;
   bool get isLoadingPhysicians => _isLoadingPhysicians;
-  List<SpecialtyModel> get specialties => _specialties;
-  List<PhysicianModel> get physicians => _physicians;
+  SpecialtiesResponse? get specialties => _specialties;
+  PhysiciansResponse? get physicians => _physicians;
   String? get error => _error;
 
   /// Charge la liste des spécialités depuis l'API
@@ -27,9 +27,9 @@ class MedicalProvider extends ChangeNotifier {
     _error = null;
     notifyListeners();
     try {
-      final resp = await _apiClient.getSpecialties();
+      SpecialtiesResponse resp = await _apiClient.getSpecialties();
       if (resp.success == true && resp.data != null) {
-        _specialties = resp.data!;
+        _specialties = resp;
       } else {
         _error = resp.message ?? 'Erreur lors du chargement des spécialités.';
       }
@@ -47,10 +47,10 @@ class MedicalProvider extends ChangeNotifier {
     _error = null;
     notifyListeners();
     try {
-      final resp = await _apiClient
+      PhysiciansResponse resp = await _apiClient
           .getPhysiciansBySpecialty({'specialty_id': specialtyId});
       if (resp.success == true && resp.data != null) {
-        _physicians = resp.data!;
+        _physicians = resp;
       } else {
         _error = resp.message ?? 'Erreur lors du chargement des médecins.';
       }
@@ -64,8 +64,8 @@ class MedicalProvider extends ChangeNotifier {
 
   /// Reset des données en mémoire si nécessaire
   void clear() {
-    _specialties = [];
-    _physicians = [];
+    _specialties = null;
+    _physicians = null;
     _error = null;
     notifyListeners();
   }
